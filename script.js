c84992877a781331e2d83a5eed092921706f2af4
unknown <mariamadiane0101@gmail.com>
2025-07-09 11:49:48 +0200

    firt commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const generatepassword=(annyway)=>{
+    const lowercase="abcdefghijklmnopqrstuvwxyz";
+    const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const nombres="0123456789";
+    const symbole="&@(){}~\_-*%£$#!";
+    const nombreHasard=lowercase+uppercase+nombres+symbole;
+
+    let password="";
+    for(let i=0;i<=annyway;i++){
+        // on declare une variable qui vas stocker un nombre alléatoire
+        // en utilisant math.floor()pour larrondissement et math.random() pour generer un nombre au hasar
+        const nombreAleatoire=Math.floor(Math.random()*nombreHasard.length);
+
+        // on concatene chaque caractere generer au hasar a la variable password
+        // pour generer un mot de passe aleatoire
+        password +=nombreHasard[nombreAleatoire];
+    }
+    return password;
+}
+const span=document.querySelector(".password span");
+span.textContent=generatepassword(10);
+
+console.log("un mot de passGenerer   "+generatepassword(10))
